11d9b9e6d8118254e416d96887986025826b61d8
alexa <alexa@alexbradys-MacBook-Pro.local>
2022-07-14 22:50:37 -0600

    refigure

--- docs/assets/js/contact_me.js
+++ docs/assets/js/contact_me.js
@@ -8,7 +8,7 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-	  var url = "https://formspree.io/" + "hellohighgroundtech@gmail.com";
+	  var url = "https://formspree.io/" + "popdemtech@gmail.com";
       var name = $("input#name").val();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
